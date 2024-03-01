b7b8b8f9d2c0ce71c2ea83334bd47c3aaa17d89e
NakulanT <nakult721@gmail.com>
2024-03-01 21:25:54 +0530

    Created files

---
+++ src/components/Auth.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Auth.css';
+
+const Auth = () => {
+    return(
+        <div>
+            <div className="Authpage">
+                None
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
---
+++ src/route.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Auth from "./components/Auth.jsx";
+
+const Routes = () => {
+    return(
+        <div>
+            <Auth />
+        </div>
+    );
+}
+
+export default Routes;
