dad3c1d2241a70af2a7897da888071ef171190b2
NakulanT <nakult721@gmail.com>
2024-03-02 20:12:03 +0530

    given bg

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import './Auth.css';
 
 const Auth = () => {
-    return(
+    return (
         <div>
-            <div className="Authpage">
-                None
+            <div className="AuthNav">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dheereshagrwal/colored-icons@1.7.4/src/app/ci.min.css" />
+                <i class="ci ci-whatsapp ci-2x"></i>
+                <h1>Whatsapp Web</h1>
+            </div>
+            <div className="AuthCard">
+                <div>
+                    <h1>Sigin in with your google account</h1>
+                    <div>
+                        contiune with google
+                    </div>
+                </div>
             </div>
         </div>
     );
