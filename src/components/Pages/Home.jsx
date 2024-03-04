796780b60ff4d9cad2762077384a53445d73359c
NakulanT <nakult721@gmail.com>
2024-03-04 21:42:51 +0530

    Added home jsx

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="HomePage">
+                <div className="Contacts"></div>
+                <div className="ContactCard"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
--- src/route.jsx
+++ src/route.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Auth from "./components/Auth.jsx";
+// import Auth from "./components/Pages/Auth.jsx";
+import Home from "./components/Pages/Home.jsx";
 
 const Routes = () => {
     return(
         <div>
-            <Auth />
+            {//<Auth />
+            }
+            <Home />
+
         </div>
     );
 }
