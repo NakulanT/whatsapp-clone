6dddafe6360572a4f1597d431d3dece66bcedb02
NakulanT <nakult721@gmail.com>
2024-03-05 21:55:37 +0530

    Added

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,6 +5,11 @@ const Home = () => {
     return (
         <div className="Home">
             <div className="HomePage">
+                <div className="Nav">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/dheereshagrwal/colored-icons@1.7.4/src/app/ci.min.css" />
+                <i className="ci ci-whatsapp ci-1x"></i>
+                <h1>Whatsapp Web</h1>
+                </div>
                 <div className="Contacts"></div>
                 <div className="ContactCard"></div>
             </div>
