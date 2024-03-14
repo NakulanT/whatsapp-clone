65b86091f17b86f7f0dcd5a3a4031ff6a1704a26
NakulanT <nakult721@gmail.com>
2024-03-14 22:04:18 +0530

    Added selector

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleLogin ,useGoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 import Home from './Home';
 import './Auth.css';
@@ -15,6 +15,8 @@ function App() {
     const [userData, setUserData] = useState(null);
     const [username, setUsername] = useState('');
     const [showAuth, setShowAuth] = useState(false);
+    const [Siginin, showSignin] = useState(false);
+    const [AlertMessage , SetAlertMessage] = useState('');
 
     const login = useGoogleLogin({
         onSuccess: (codeResponse) => setUser(codeResponse),
@@ -39,7 +41,7 @@ function App() {
                         email: res.data.email,
                         picture: res.data.picture
                     });
-                    
+
                 } catch (error) {
                     console.log(error);
                 } finally {
@@ -49,7 +51,7 @@ function App() {
         };
 
         fetchProfile();
-    }, [user , userData]);
+    }, [user, userData]);
 
     const handleUsernameChange = (e) => {
         setUsername(e.target.value);
@@ -65,26 +67,26 @@ function App() {
                 // Check if the email is already registered in the members collection
                 const emailRef = ref(db, `members/${encodeEmail(userData.email)}`);
                 const emailSnapshot = await get(emailRef);
-    
+
                 // Check if the username already exists in the Users collection
                 const usernameRef = ref(db, `Users/${username}`);
                 const usernameSnapshot = await get(usernameRef);
-    
+
                 if (!emailSnapshot.exists() && usernameSnapshot.exists()) {
                     alert("Username is not registered. Please sign up.");
                     setShowAuth(false);
                 } else if (emailSnapshot.exists()) {
                     setShowAuth(true);
-                } else if (!emailSnapshot.exists() && !usernameSnapshot.exists()){
-                    
+                } else if (!emailSnapshot.exists() && !usernameSnapshot.exists()) {
+
                     await set(emailRef, {
                         email: userData.email
                     });
-    
+
                     await set(usernameRef, {
                         email: userData.email,
                     });
-    
+
                     setShowAuth(true);
                 }
             }
@@ -92,7 +94,7 @@ function App() {
             console.error("Error:", error);
         }
     };
-    
+
 
     if (user && userData && showAuth) {
         return <Home name={userData.name} email={userData.email} picture={userData.picture} username={username} />;
@@ -108,24 +110,42 @@ function App() {
                     <h1>Whatsapp Web</h1>
                 </div>
                 <div className="AuthCard">
-                    <h1>Sign in with your Google account</h1>
-                    <GoogleLogin
-                        clientId="692055383262-r0t5oab7muk0fee2lgk39a5tpgdilmm7.apps.googleusercontent.com"
-                        buttonText="Sign in with Google 🚀"
-                        onSuccess={login}
-                        onFailure={login}
-                    />
-                    {!showAuth && (
-                        <form>
-                            <input
-                                type="text"
-                                placeholder="Enter username"
-                                value={username}
-                                onChange={handleUsernameChange} // Listen for changes in the input field
+
+                    <div className='CardSelector'>
+                    <a id="Signup" className={Siginin ? 'active' : ''} onClick={() => showSignin(false)}>Sign Up</a>
+                    <a id="Signin" className={!Siginin ? 'active' : ''} onClick={() => showSignin(true)}>Sign In</a>
+
+                    </div>
+                    <div className='AlertMessages'>{AlertMessage}</div>
+                    {Siginin ? (
+                        <div className='Siginin'>
+                            <GoogleLogin
+                                clientId="692055383262-r0t5oab7muk0fee2lgk39a5tpgdilmm7.apps.googleusercontent.com"
+                                buttonText="Sign in with Google 🚀"
+                                onSuccess={login}
+                                onFailure={login}
                             />
-                            <button type="button" onClick={Continue}>Sign Up</button>
-                        </form>
+                        </div>
+                    ) : (
+                        <div className='Siginup'>
+                            <form>
+                                <input
+                                    type="text"
+                                    placeholder="Enter username"
+                                    value={username}
+                                    onChange={handleUsernameChange} // Listen for changes in the input field
+                                />
+                                <GoogleLogin
+                                clientId="692055383262-r0t5oab7muk0fee2lgk39a5tpgdilmm7.apps.googleusercontent.com"
+                                buttonText="Sign in with Google 🚀"
+                                onSuccess={login}
+                                onFailure={login}
+                            />
+                                <button type="button" onClick={Continue}>Register</button>
+                            </form>
+                        </div>
                     )}
+
                 </div>
             </div>
         );
